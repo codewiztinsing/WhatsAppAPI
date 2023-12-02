b302e428e3fd9820109142a4c791ceea83db3db4
codewiztinsing <tinsingjobs2k@gmail.com>
2023-12-02 19:58:45 +0300

    modifing urls

--- WhatsAppAPI/urls.py
+++ WhatsAppAPI/urls.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("chat.controller.chat_rooms"))
+    path('',include("chat.controller.chat_rooms")),
     path('api/schema/',SpectacularAPIView.as_view(),name="schema"),
     path("api/schema/docs",SpectacularSwaggerView.as_view(url_name = "schema")),
 ]
