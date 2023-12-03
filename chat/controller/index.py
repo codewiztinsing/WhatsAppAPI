73b16c37cc0c154d02d7215202ca944afebb97f7
codewiztinsing <tinsingjobs2k@gmail.com>
2023-12-03 04:16:48 +0300

    adding message

--- chat/controller/index.py
+++ chat/controller/index.py
@@ -1,3 +1,12 @@
+from django.urls import path
+# from chat.services.room import chatroom_list,create_chatroom,chat_room
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from chat.services.message  import MessageViewSet
+from chat.services.room  import ChatRoomViewSet
+
+
+
 # app_name = 'chat'
 router = DefaultRouter()
 router.register(r'messages', MessageViewSet, basename='messages')
@@ -8,3 +17,4 @@ urlpatterns = [
     path('', include(router.urls))
   
 ]
+
