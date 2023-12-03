0f43dd29e41574d54ebd0d3aff90e1421de24f69
codewiztinsing <tinsingjobs2k@gmail.com>
2023-12-03 04:25:15 +0300

    adding serializers

--- WhatsAppAPI/urls.py
+++ WhatsAppAPI/urls.py
@@ -8,7 +8,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("chat.controller.chat_rooms")),
-    
+    path('api/v1/',include("chat.controller.index")),
     path('api/schema/',SpectacularAPIView.as_view(),name="schema"),
     path("api/schema/docs",SpectacularSwaggerView.as_view(url_name = "schema")),
 ]
